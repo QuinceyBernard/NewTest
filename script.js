afd9d06e4afecf4194428c1da97aef9b4b8ce901
QuinceyBernard <quincey.bernard97@gmail.com>
2020-03-02 14:20:49 +0000

    Update - 2nd March Included an trigger dependent on gender detected. Removed the detection boxes drawn on faces

--- script.js
+++ script.js
@@ -25,17 +25,36 @@ video.addEventListener('play', () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    // faceapi.draw.drawDetections(canvas, resizedDetections)
+    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     resizedDetections.forEach(detection => {
-      const box = detection.detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age)+"years old" + detection.gender})
-      drawBox.draw(canvas)
-      // const emotions =
-      // console.log(detections[0].expressions)
-      const emotional = detections[0].expressions
-      console.log(emotional)
+      // const box = detection.detection.box
+      // const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age)+"years old" + detection.gender})
+      // drawBox.draw(canvas)
+
+      var genders = detections[0].gender
+      // console.log(genders)
+      if (genders === 'female'){
+        console.log("Hello Woman")
+        var f = document.getElementById("myCanvas");
+        var ftx = f.getContext("2d");
+        ftx.beginPath();
+        ftx.rect(0,0,150,150);
+        ftx.stroke();
+        ftx.fillStyle = "red";
+        ftx.fill();
+      } else if (genders === 'male'){
+        console.log("Hello Man")
+        var m = document.getElementById("myCanvas");
+        var mtx = m.getContext("2d");
+        mtx.beginPath();
+        mtx.rect(0,0,150,150);
+        mtx.stroke();
+        mtx.fillStyle = "blue";
+        mtx.fill();
+      }
+
 
     })
   }, 1000)
